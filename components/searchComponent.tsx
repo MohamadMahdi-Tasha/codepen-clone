c81e5894cee8441ac0151e54b9896d42a74a3ca2
Mahdi Tasha <imwhdiiii@gmail.com>
2023-09-02 15:59:27 +0330

    added styles of last commit\

--- components/searchComponent.tsx
+++ components/searchComponent.tsx
@@ -19,31 +19,31 @@ export default function SearchComponent():ReactNode {
                 <label htmlFor={"search-input"}>
                     <IconComponent name={'search'} className={'text-dropdownBg'} />
                 </label>
-                <input onFocus={() => setInputFocused(true)} onBlur={() => setInputFocused(false)} autoComplete={'false'} className={'bg-transparent placeholder:text-dropdownBg placeholder:text-[20px] text-[20px] text-white placeholder:font-bold w-full outline-0'} required type="search" id={'search-input'} placeholder={'Search CodePen...'} />
+                <input onFocus={() => setInputFocused(true)} onBlur={() => setInputFocused(false)} autoComplete={'off'} className={'bg-transparent placeholder:text-dropdownBg placeholder:text-[20px] text-[20px] text-white placeholder:font-bold w-full outline-0'} required type="search" id={'search-input'} placeholder={'Search CodePen...'} />
             </form>
-            <div data-opened={isInputFocused}>
-                <Link href={'your-work'}>
-                    <button tabIndex={-1}>
-                        <IconComponent name={'person'} />
-                        <span>Your Work</span>
+            <div data-opened={isInputFocused} className={'absolute flex gap-[20px] top-[110%] rounded-[10px] left-0 shadow-lg p-[10px] w-full overflow-x-auto bg-mainBg transition-all duration-500 data-[opened="true"]:visible data-[opened="true"]:opacity-100 data-[opened="true"]:translate-y-0 data-[opened="false"]:invisible data-[opened="false"]:opacity-0 data-[opened="false"]:translate-y-[-20px]'}>
+                <Link href={'/your-work'} className={'block shrink-0'}>
+                    <button className={'flex gap-[10px] bg-dropdownBg/30 items-center px-[10px] py-[5px] rounded-[3px] hover:bg-dropdownBg transition-all [&>*]:hover:text-white'} tabIndex={-1}>
+                        <IconComponent name={'person'} className={'text-dropdownBg/90 transition-all'} />
+                        <span className={'text-[16px] whitespace-nowrap font-normal text-dropdownBg/90 transition-all'}>Your Work</span>
                     </button>
                 </Link>
-                <Link href={'your-work'}>
-                    <button tabIndex={-1}>
-                        <IconComponent name={'pen'} />
-                        <span>Pens</span>
+                <Link href={'/search/pens'} className={'block shrink-0'}>
+                    <button className={'flex gap-[10px] bg-dropdownBg items-center px-[10px] py-[5px] rounded-[3px]'} tabIndex={-1}>
+                        <IconComponent name={'pen'} className={'text-blue'} />
+                        <span className={'text-[16px] whitespace-nowrap text-white font-normal'}>Pens</span>
                     </button>
                 </Link>
-                <Link href={'your-work'}>
-                    <button tabIndex={-1}>
-                        <IconComponent name={'project'} />
-                        <span>Projects</span>
+                <Link href={'/search/projects'} className={'block shrink-0'}>
+                    <button className={'flex gap-[10px] bg-dropdownBg/30 items-center px-[10px] py-[5px] rounded-[3px] hover:bg-dropdownBg transition-all [&>*]:hover:text-white'} tabIndex={-1}>
+                        <IconComponent name={'project'} className={'text-dropdownBg/90 transition-all'} />
+                        <span className={'text-[16px] whitespace-nowrap font-normal text-dropdownBg/90 transition-all'}>Projects</span>
                     </button>
                 </Link>
-                <Link href={'your-work'}>
-                    <button tabIndex={-1}>
-                        <IconComponent name={'collection'} />
-                        <span>Collections</span>
+                <Link href={'/search/collections'} className={'block shrink-0'}>
+                    <button className={'flex gap-[10px] bg-dropdownBg/30 items-center px-[10px] py-[5px] rounded-[3px] hover:bg-dropdownBg transition-all [&>*]:hover:text-white'} tabIndex={-1}>
+                        <IconComponent name={'collection'} className={'text-dropdownBg/90 transition-all'} />
+                        <span className={'text-[16px] whitespace-nowrap font-normal text-dropdownBg/90 transition-all'}>Collections</span>
                     </button>
                 </Link>
             </div>
