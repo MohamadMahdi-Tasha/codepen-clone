9217b0b3925788e2ade4d829a8d5f650b89ab07d
Mahdi Tasha <imwhdiiii@gmail.com>
2023-09-03 15:59:32 +0330

    added new page of user profile

--- app/[username]/page.tsx
+++ app/[username]/page.tsx
@@ -2,12 +2,12 @@
 // Importing Part
 import {ReactNode} from 'react';
 
-// Creating And Exporting Pen Projects As Default
-export default function UserPenPage():ReactNode {
+// Creating And Exporting User Profile As Default
+export default function UserProfilePage():ReactNode {
     // Returning JSX
     return (
         <div>
-            <h1>User Pen Page</h1>
+            <h1>User Profile Page</h1>
         </div>
     );
 }
