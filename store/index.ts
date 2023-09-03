937996e1c2266ceac4ef316609e8f249641d1c0b
Mahdi Tasha <imwhdiiii@gmail.com>
2023-09-03 16:28:58 +0330

    added ssot

---
+++ store/index.ts
@@ -0,0 +1,135 @@
+// Codes By Mahdi Tasha
+// Importing Part
+import {configureStore, createSlice, Slice} from "@reduxjs/toolkit";
+import ProfileImage from '@/public/assets/img/img-profile.jpg';
+
+// Creating Slice
+const appSlice:Slice = createSlice({
+    name: 'App',
+    initialState: {
+        users: [
+            {
+                profileImage: ProfileImage.src,
+                name: 'Mahdi Tasha',
+                id: 'tashaDev',
+                location: 'Iran, Tehran',
+                bio: 'junior font end developer :))',
+                links: [
+                    'https://www.instagram.com/mahditasha_',
+                    'https://www.github.com/MohamadMahdi-Tasha'
+                ],
+                following: [
+                    {
+                        profileImage: ProfileImage.src,
+                        name: 'NoBody',
+                        id: 'Anonymous',
+                        location: 'Heaven',
+                        followersCount: 1,
+                        followingCount: 1,
+                        bio: 'Hacker with 100 years of experience',
+                        links: [],
+                        pens: [
+                            {
+                                isPickedByCodePen: false,
+                                htmlCode: '<img src="https://upload.wikimedia.org/wikipedia/commons/a/a6/Anonymous_emblem.svg" alt="Logo Of Anonymous Group" />',
+                                cssCode: 'img {width: 100px;height: 100px;border-radius: 100%}',
+                                name: 'Expect Us | Anonymous',
+                                likesCount: 1002344234217,
+                                commentsCount: 112345687,
+                                viewCount: 53100003240006,
+                            }
+                        ]
+                    }
+                ],
+                followers: [],
+                pens: [
+                    {
+                        isPickedByCodePen: true,
+                        htmlCode: '<h1>Waterfall Fancy</h1>',
+                        cssCode: 'h1 {font-weight: 700;color: red;text-align:center;}',
+                        name: 'Waterfall Fancy',
+                        likesCount: 536,
+                        commentsCount: 536,
+                        viewCount: 536,
+                    }
+                ]
+            },
+            {
+                profileImage: ProfileImage.src,
+                name: 'NoBody',
+                id: 'Anonymous',
+                location: 'Heaven',
+                following: [],
+                followers: [
+                    {
+                        profileImage: ProfileImage.src,
+                        name: 'Mahdi Tasha',
+                        id: 'tashaDev',
+                        location: 'Iran, Tehran',
+                        bio: 'junior font end developer :))',
+                        links: [
+                            'https://www.instagram.com/mahditasha_',
+                            'https://www.github.com/MohamadMahdi-Tasha'
+                        ],
+                        following: [
+                            {
+                                profileImage: ProfileImage.src,
+                                name: 'NoBody',
+                                id: 'Anonymous',
+                                location: 'Heaven',
+                                followersCount: 1,
+                                followingCount: 1,
+                                bio: 'Hacker with 100 years of experience',
+                                links: [],
+                                pens: [
+                                    {
+                                        isPickedByCodePen: false,
+                                        htmlCode: '<img src="https://upload.wikimedia.org/wikipedia/commons/a/a6/Anonymous_emblem.svg" alt="Logo Of Anonymous Group" />',
+                                        cssCode: 'img {width: 100px;height: 100px;border-radius: 100%}',
+                                        name: 'Expect Us | Anonymous',
+                                        likesCount: 1002344234217,
+                                        commentsCount: 112345687,
+                                        viewCount: 53100003240006,
+                                    }
+                                ]
+                            }
+                        ],
+                        followers: [],
+                        pens: [
+                            {
+                                isPickedByCodePen: true,
+                                htmlCode: '<h1>Waterfall Fancy</h1>',
+                                cssCode: 'h1 {font-weight: 700;color: red;text-align:center;}',
+                                name: 'Waterfall Fancy',
+                                likesCount: 536,
+                                commentsCount: 536,
+                                viewCount: 536,
+                            }
+                        ]
+                    },
+                ],
+                bio: 'Hacker with 100 years of experience',
+                links: [],
+                pens: [
+                    {
+                        isPickedByCodePen: false,
+                        htmlCode: '<img src="https://upload.wikimedia.org/wikipedia/commons/a/a6/Anonymous_emblem.svg" alt="Logo Of Anonymous Group" />',
+                        cssCode: 'img {width: 100px;height: 100px;border-radius: 100%}',
+                        name: 'Expect Us | Anonymous',
+                        likesCount: 1002344234217,
+                        commentsCount: 112345687,
+                        viewCount: 53100003240006,
+                    }
+                ]
+            }
+        ]
+    },
+    reducers: {}
+})
+
+// Exporting Actions To Use
+export const actionsOfAppSlice = appSlice.actions;
+
+// Configuring Store And Exporting It As Default
+const appState = configureStore({reducer: appSlice.reducer})
+export default appState;
