0aa8e415bbd71526b3d9ee68fcb518bda3948450
Mahdi Tasha <imwhdiiii@gmail.com>
2023-09-03 15:58:14 +0330

    added user pen page file

---
+++ app/[username]/[pen]/page.tsx
@@ -0,0 +1,13 @@
+// Codes By Mahdi Tasha
+// Importing Part
+import {ReactNode} from 'react';
+
+// Creating And Exporting Pen Projects As Default
+export default function UserPenPage():ReactNode {
+    // Returning JSX
+    return (
+        <div>
+            <h1>User Page</h1>
+        </div>
+    );
+}
